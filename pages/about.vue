a0dfa7a108d73bb84d78f545a7183087e719d8a2
Anthony Da Mota <anthonyk.damota@gmail.com>
2018-12-16 17:25:59 +0100

    Added changelog

--- pages/about.vue
+++ pages/about.vue
@@ -3,8 +3,25 @@
     section.container.with-title
       h2.title About
 
-      p By&nbsp;
-        a(:href='github' target='_blank') {{name}}
+      h3 Changelog
+
+      ul.changelog
+        li.version v1.0.2
+          ul.changes
+            li.bugfix Fixed XAJ4007 serial numbers
+            li.feature Added a changelog to the about page, so you can track what's going on here :)
+        li.version v1.0.1
+          ul.changes
+            li.bugfix Dramatically updated the algorithm thanks to the help of the community
+        li.version v1.0.0
+          ul.changes
+            li.initial Initial public release with multiple SN checker and barcode scanner
+
+      hr
+
+      h3 Who made this?
+        p Me!
+          a(:href='github' target='_blank') {{name}}
 </template>
 
 <script>
@@ -21,5 +38,47 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus">
+ul {
+  &.changelog, &.changes {
+    list-style: none;
+    margin-left: 0;
+    padding-left: 0;
+  }
+
+  li {
+    &:before {
+      padding-right: 10px;
+    }
+
+    &.version {
+      &:before {
+        content: '🔖';
+      }
+
+      margin-top: 25px;
+    }
+
+    &.feature {
+      &:before {
+        content: '✨';
+      }
+    }
+
+    &.bugfix {
+      &:before {
+        content: '🔧';
+      }
+    }
+
+    &.initial {
+      &:before {
+        content: '🎉';
+      }
+    }
+
+    padding-left: 1em;
+    text-indent: -1em;
+  }
+}
 </style>
