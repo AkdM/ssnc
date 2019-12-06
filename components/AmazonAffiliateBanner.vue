338982c31b0ff02f55bd9ac97a1460d8031ffcde
Anthony Da Mota <AkdM@users.noreply.github.com>
2019-12-06 17:54:43 +0100

    Amazon dynamic widget link

--- components/AmazonAffiliateBanner.vue
+++ components/AmazonAffiliateBanner.vue
@@ -1,15 +1,17 @@
-<template>
-  <section class="nes-container with-title">
-    <h2 class="title">Support me</h2>
-    <iframe
-      src="https://rcm-eu.amazon-adsystem.com/e/cm?o=8&p=288&l=ez&f=ifr&linkID=ca38d3ac168d43f0ef31a2d02849e7d6&t=damota03-21&tracking_id=damota03-21"
-      width="320"
-      height="50"
-      scrolling="no"
-      border="0"
-      marginwidth="0"
-      style="border:none;"
-      frameborder="0"
-    />
-  </section>
+<template lang="pug">
+  section.nes-container.with-title
+    h2.title Support me
+    script(charset="utf-8" type="text/javascript" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fdamota03-21%2F8009%2Fe03acfc2-c344-423e-893e-d09ad8e759bd&Operation=GetScriptTemplate")
+    noscript
+      a(rel="nofollow", href="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fdamota03-21%2F8009%2Fe03acfc2-c344-423e-893e-d09ad8e759bd&Operation=NoScript") Buy at Amazon - Achetez sur Amazon !
+    //- <iframe
+    //- src="https://rcm-eu.amazon-adsystem.com/e/cm?o=8&pP=288&l=ez&f=ifr&linkID=ca38d3ac168d43f0ef31a2d02849e7d6&t=damota03-21&tracking_id=damota03-21"
+    //- width="320"
+    //- height="50"
+    //- scrolling="no"
+    //- border="0"
+    //- marginwidth="0"
+    //- style="border:none;"
+    //- frameborder="0"
+    //- />
 </template>
